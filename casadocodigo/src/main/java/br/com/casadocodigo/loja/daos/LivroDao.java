da0846cfc40a2f33789f11eda16d91cee733886a
Franklin de Azeredo <fkazeredo.dev@gmail.com>
2020-05-30 03:11:33 -0300

    Módulo 1 terminado

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/daos/LivroDao.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/daos/LivroDao.java
@@ -17,9 +17,30 @@ public class LivroDao {
 	}
 
 	public List<Livro> listar() {
-		String jpql = "select distinct(l) from Livro l "
+		String jpql = "select distinct(l) from Livro l order by l.id desc"
 				+ "join fetch l.autores";
 		return this.manager.createQuery(jpql, Livro.class).getResultList();
+	}
+
+	public List<Livro> ultimosLancamentos() {
+		String jpql = "select l from Livro l order by l.id desc";
+		return this.manager.createQuery(jpql, Livro.class)
+				.setMaxResults(3)
+				.getResultList();
+	}
+
+	public List<Livro> demaisLivros() {
+		String jpql = "select l from Livro l order by l.id desc";
+		return this.manager.createQuery(jpql, Livro.class)
+				.setFirstResult(3)
+				.getResultList();
+	}
+
+	public Livro buscarPorId(Integer id) {
+		String jpql = "select l from Livro l join fetch l.autores where l.id = :id";
+		return this.manager.createQuery(jpql, Livro.class)
+				.setParameter("id", id)
+				.getSingleResult();
 	}
 
 }
